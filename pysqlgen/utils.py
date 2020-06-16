efaad4010fe416bbfbc1923fe51eac8030e2258e
Alex Bird <abird@turing.ac.uk>
2020-06-16 18:17:53 +0100

    Squashed 'pySQLGen/' changes from 3989740..4d05119
    4d05119 avoid bug when referencing two of the same field in a query
    fb861ea plumb in 'name' checkboxes/get concept names into backend
    9e1ab12 plumb-in checkboxes into GUI
    3aae2e2 remake UserOptions class, under-the-hood updates to generation, add 'name' column to UI, but not plumbed to SQL yet
    
    git-subtree-dir: pySQLGen
    git-subtree-split: 4d05119ca6e610d59bd26b981747d3fa3e1fa034

---
+++ pysqlgen/utils.py
@@ -0,0 +1,61 @@
+from .dbtree import SchemaNode
+import re
+
+
+def is_node(x, allow_custom=False):
+    if isinstance(x, SchemaNode):
+        return True
+    elif allow_custom and isinstance(x, str):
+        return x.lower() == "custom"
+    else:
+        return False
+
+
+def node_isin_context(x, context, allow_custom=False, allow_None=False):
+    if x in context.nodes:
+        return True
+    elif allow_None and x is None:
+        return True
+    elif allow_custom and isinstance(x, str) and x.lower() == 'custom':
+        return True
+    else:
+        return False
+
+
+def in_list(x, L, allow_None=False):
+    if allow_None and x is None:
+        return True
+    return x in L
+
+
+def str_to_fieldname(x):
+    return x.strip().replace(' ', '_')
+
+
+def make_unique_name(x, *existing_names):
+    x = x.lower()
+    name = x[:1]
+    a_ix = 0
+    for cur in existing_names:
+        cur = cur.values() if isinstance(cur, dict) else cur
+        while name in cur:
+            a_str = '' if a_ix == 0 else str(a_ix)
+            a_ix += 1
+            name = ''.join([n[0].lower() for n in x.split("_")]) + a_str
+    return name
+
+
+def sync_index(i, cur_list, dest_list, None_is_str=False):
+    cur_item = cur_list[i]
+    cur_item = None if (None_is_str and (cur_item is None)) else cur_item
+    return dest_list.index(cur_item)
+
+
+def get_nth_chunk(n, indexable, chunksizes):
+    chunk_start = sum(chunksizes[:n])
+    chunk_end = chunk_start + chunksizes[n]
+    return indexable[chunk_start:chunk_end]
+
+
+def rm_alias_placeholder(x):
+    return re.sub('{alias(:s)?}', '', x)
